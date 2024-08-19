8c25b76090bd1e64d46c956ecc33e5fecdaf79d5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-19 14:58:21 +0530

    added express and defined prot

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(post, () => console.log(`server is running on the port ${port}`));
