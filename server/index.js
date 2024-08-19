5a73bce15737f772d565200fa79b4a8cb25133c6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-19 15:09:30 +0530

    required dotenv config

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
