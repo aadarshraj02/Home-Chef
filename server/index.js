cf4c3e9f2d0c40cc4d253ac2bc9e030e86ad26d8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-19 15:01:44 +0530

    defined port and cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.listen(post, () => console.log(`server is running on the port ${port}`));
