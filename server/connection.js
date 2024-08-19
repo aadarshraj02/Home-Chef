c2f1609b04d789b9eafcb35e7ecf2a5ea93288c5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-19 21:03:01 +0530

    added connection to mongo

---
+++ server/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    if (connection.STATES.connected) console.log("Database Connected");
+    else console.log("Connection Failed");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = { connectDB };
