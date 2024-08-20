1350dd2177437d1b791acd89f6ac930bae6ff873
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 13:24:57 +0530

    passed origin url and credentials to cors

--- server/index.js
+++ server/index.js
@@ -10,10 +10,15 @@ const { connectDB } = require("./connection");
 connectDB();
 
 const routes = require("./routes");
-app.use("/api",routes)
+app.use("/api", routes);
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.listen(port, () => console.log(`Server is running on the port ${port}`));
