0cc766189bc5dc8556acfd5c53d783654d948e9c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 14:33:58 +0530

    added function to check user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -95,3 +95,25 @@ exports.logout = async (req, res) => {
     });
   }
 };
+
+exports.checkUser = async (req, res) => {
+  const id = req.id;
+  try {
+    const user = await User.findById(id).select("-password");
+
+    if (!user)
+      return res.status(400).json({
+        success: false,
+        message: "Please Signup",
+      });
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
