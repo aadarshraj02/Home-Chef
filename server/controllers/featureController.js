7455fc787ddbdafefa5f558b8e34ca7952f4e425
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 21:27:21 +0530

    added function to remove from fav

--- server/controllers/featureController.js
+++ server/controllers/featureController.js
@@ -37,3 +37,31 @@ exports.addToFavorites = async (req, res) => {
     });
   }
 };
+
+exports.removeFromFavorites = async (req, res) => {
+  const { id } = req.params;
+  const favorite = req.body;
+
+  try {
+    let user = await User.findById(id);
+    if (!user)
+      return res.status(404).json({
+        success: false,
+        message: "User not Found",
+      });
+
+    user.favorites = user.favorites.filter(
+      (fav) => fav.idMeal !== favorite.idMeal
+    );
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "Recipe removed from favorites",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
