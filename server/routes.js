682958bd5c80d2cf8ecba03c4bb25616301b9d49
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 21:34:20 +0530

    added routes to add fav,getfav and remove from fav

--- server/routes.js
+++ server/routes.js
@@ -7,10 +7,18 @@ const {
   checkUser,
 } = require("./controllers/authController");
 const { verifyToken } = require("./middlewares/verifyToken");
+const {
+  addToFavorites,
+  removeFromFavorites,
+  getFavorites,
+} = require("./controllers/featureController");
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/checkUser", verifyToken, checkUser);
+router.post("/addToFavorites/:id", addToFavorites);
+router.post("/removeFromFavorites/:id", removeFromFavorites);
+router.get("/getFavorites/:id", getFavorites);
 
 module.exports = router;
