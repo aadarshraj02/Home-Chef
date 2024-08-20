7b79ee061c8b07fb8847ec3c140ec680884794f5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 22:06:47 +0530

    added routes for RecipeDetails

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import RecipeDetails from "./pages/RecipeDetails";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/recipeDetails" element={<RecipeDetails />} />
         </Routes>
       </BrowserRouter>
     </>
