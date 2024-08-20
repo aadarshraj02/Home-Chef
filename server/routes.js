214bfb4589dc196f8312fa8827abb08bd5344e10
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 20:46:19 +0530

    added routes for check userq

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login, logout } = require("./controllers/authController");
+const {
+  signup,
+  login,
+  logout,
+  checkUser,
+} = require("./controllers/authController");
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", logout);
+router.get("/checkUser", checkUser);
 
 module.exports = router;
