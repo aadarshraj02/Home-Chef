e7b6ebbba13c018230fae40feb4b35ab5f5ca3f5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 21:30:20 +0530

    added function to get all afv

--- server/controllers/featureController.js
+++ server/controllers/featureController.js
@@ -65,3 +65,25 @@ exports.removeFromFavorites = async (req, res) => {
     });
   }
 };
+
+exports.getFavorites = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let user = await User.findById(id);
+    if (!user)
+      return res.status(404).json({
+        success: false,
+        message: "User not Found",
+      });
+    return res.status(200).json({
+      success: true,
+      favorites: user.favorites,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
