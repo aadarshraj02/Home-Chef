961fd12abfe7ad1d702210d9116bd8f3b30152e0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-20 12:17:27 +0530

    added signup

---
+++ server/controllers/authController.js
@@ -0,0 +1,35 @@
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+exports.signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    let user;
+    user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "Please signup",
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    user = await User.create({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    await user.save();
+    return res.status(201).json({
+      success: true,
+      message: "Signup Successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
