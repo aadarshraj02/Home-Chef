fdbcf88c6b0e38a2187c3fbbc377f6f6955f1064
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-21 16:07:26 +0530

    styled nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,10 +6,19 @@ const Navbar = () => {
       <Link to="/" className="TITLE_TEXT text-xl font-bold text-red-500">
         HomeChef
       </Link>
-      <div className="flex gap-5">
-        <Link to="">About</Link>
-        <Link to="">Login</Link>
-        <Link to="">Signup</Link>
+      <div className="flex gap-5 items-center justify-center text-zinc-600 ">
+        <Link to="/about" className="hover:text-black duration-300 ease-linear">
+          About
+        </Link>
+        <Link to="/login" className="hover:text-black duration-300 ease-linear">
+          Login
+        </Link>
+        <Link
+          to="/signup"
+          className="hover:text-black duration-300 ease-linear"
+        >
+          Signup
+        </Link>
       </div>
     </nav>
   );
