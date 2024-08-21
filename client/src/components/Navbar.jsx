a1aaa7a038b825d8acdbd6b5fc22c46d787e60cd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-21 15:58:11 +0530

    styled nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="flex justify-between bg-white items-center px-5 py-4 shadow-md mb-10">
+      <Link to="/" className="TITLE_TEXT text-xl font-bold text-red-500">
+        HomeChef
+      </Link>
+      <div className="flex gap-5">
+        <Link to="">About</Link>
+        <Link to="">Login</Link>
+        <Link to="">Signup</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
