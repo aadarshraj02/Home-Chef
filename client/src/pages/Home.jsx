0c21b2d0399121f1ad0f3482ec161537045266ac
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-21 20:10:18 +0530

    updated responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
           />
         </form>
       </div>
-      <div className="my-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto gap-3">
+      <div className="my-10 w-[40vw] sm:w-[60vw] lg:[70vw] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center mx-auto  gap-3">
         {recipes.map((recipe) => (
           <RecipeCard
             key={recipe.idMeal}
