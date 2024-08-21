c8d7e83eef7648f53e519ec24cf3daf28fdbe0a0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-21 20:03:42 +0530

    passed id title image as props

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -1,18 +1,18 @@
 import { IoIosHeart } from "react-icons/io";
 
-const RecipeCard = () => {
+const RecipeCard = ({ id, image, title }) => {
   return (
     <div className="flex flex-col justify-between bg-white p-3 rounded-lg shadow-md">
       <div className="overflow-hidden">
         <img
-          src="https://www.themealdb.com/images/media/meals/sbx7n71587673021.jpg"
-          alt=""
+          src={image}
+          alt={title}
           className="rounded-lg hover:scale-110 transition-all duration-300 ease-linear"
           width={250}
         />
       </div>
       <div className="flex justify-between items-center mt-3">
-        <h3>Chick-Fil-A Sandwich</h3>
+        <h3>{title}</h3>
         <IoIosHeart className="text-red-500 text-lg hover:scale-125 transition-all duration-300 ease-linear cursor-pointer" />
       </div>
     </div>
