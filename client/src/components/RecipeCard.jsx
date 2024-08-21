d7d457851e6e4b6dade8afd8edb74824e8de7211
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-21 18:49:45 +0530

    added a div and image

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -1,5 +1,15 @@
 const RecipeCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img
+          src="https://www.themealdb.com/images/media/meals/sbx7n71587673021.jpg"
+          alt=""
+          width={250}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RecipeCard;
