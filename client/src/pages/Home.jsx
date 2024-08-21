96d8119b3879cf0a885ac96527181973600d14ce
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-21 22:08:36 +0530

    added spinner on loading screen

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,42 @@
 import { useEffect, useState } from "react";
 import RecipeCard from "../components/RecipeCard";
 import axios from "axios";
+import { PacmanLoader } from "react-spinners";
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getRecipes = async (text) => {
-    const res = await axios.get(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${text}`
-    );
-    const data = await res.data;
-    setRecipes(data.meals);
+    setLoading(true);
+    try {
+      const res = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${text}`
+      );
+      const data = res.data;
+      setRecipes(data.meals);
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+      setRecipes([]);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const getInitialRecipes = async () => {
-    const res = await axios.get(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?i=`
-    );
-    const data = await res.data;
-    if (data === null || data === undefined) {
-      console.log("No data Found");
-    } else setRecipes(data.meals);
+    setLoading(true);
+    try {
+      const res = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?i=`
+      );
+      const data = res.data;
+      setRecipes(data.meals || []);
+    } catch (error) {
+      console.error("Error fetching initial recipes:", error);
+      setRecipes([]);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -41,15 +57,21 @@ const Home = () => {
           />
         </form>
       </div>
-      <div className="my-10 w-[40vw] sm:w-[60vw] lg:[70vw] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 items-center mx-auto  gap-3">
-        {recipes.map((recipe) => (
-          <RecipeCard
-            key={recipe.idMeal}
-            id={recipe.idMeal}
-            title={recipe.strMeal}
-            image={recipe.strMealThumb}
-          />
-        ))}
+      <div className="my-10 w-[40vw] sm:w-[60vw] lg:w-[70vw] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 items-center mx-auto gap-3">
+        {loading ? (
+          <PacmanLoader />
+        ) : recipes && recipes.length > 0 ? (
+          recipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.idMeal}
+              id={recipe.idMeal}
+              title={recipe.strMeal}
+              image={recipe.strMealThumb}
+            />
+          ))
+        ) : (
+          <h1>No Recipes to show</h1>
+        )}
       </div>
     </div>
   );
