dbd85406b188281bf5397313feb73b01564805d4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-22 20:07:24 +0530

    added signup form

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,80 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center h-[80vh]">
+      <div className="w-full max-w-md">
+        <form className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4">
+          <div className="text-gray-800 text-2xl flex justify-center border-b-2 py-2 mb-4">
+            Create Account
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="username"
+              type="text"
+              required
+              placeholder="username"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="email"
+              type="email"
+              required
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              placeholder="Password"
+              name="password"
+              required
+              autoComplete="false"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="px-4 py-2 rounded text-white inline-block shadow-lg bg-red-500 hover:bg-red-600 focus:bg-red-700"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+          <div className="text-xs flex gap-2 mt-2">
+            <span>Or</span>
+            <Link to="/login" className="text-red-500 hover:underline">
+              Login to existing account
+            </Link>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+          &copy;2024 HomeChef. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
