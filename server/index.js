ca961ad30326c90fc79167797e549493cad9fc77
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-23 17:44:07 +0530

    fixed error with login

--- server/index.js
+++ server/index.js
@@ -7,18 +7,20 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const { connectDB } = require("./connection");
+
 connectDB();
 
-const routes = require("./routes");
-app.use("/api", routes);
-
-app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.use(express.json());
 app.use(cookieParser());
 
-app.listen(port, () => console.log(`Server is running on the port ${port}`));
+const routes = require("./routes");
+app.use("/api", routes);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
