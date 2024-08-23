d57eec3ee16c6d58d9c584bc442a187c87215e33
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-23 15:09:45 +0530

    added authSlice

---
+++ client/redux/slices/authSlice.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isAuth: CSSFontFeatureValuesRule,
+    user: [],
+  },
+  reducers: {
+    login: (state) => {
+      state.isAuth = true;
+    },
+    logout: (state) => {
+      state.isAuth = false;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { login, logout, setUser } = authSlice.actions;
+export default authSlice.reducer;
