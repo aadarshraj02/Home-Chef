f5d6505e49dc5bd1533474b3083ae9e8f1f5ee9b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-23 15:25:12 +0530

    passed user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -70,6 +70,7 @@ exports.login = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Login Successful",
+      user,
     });
   } catch (error) {
     return res.status(500).json({
